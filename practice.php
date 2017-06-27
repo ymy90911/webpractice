344a2388887251537a77a0f00681b231e103e4f5
ymy90911 <ymy90911@hotmail.com>
2017-06-27 15:46:43 -0500

    front page copy without cssing

--- practice.php
+++ practice.php
@@ -13,8 +13,10 @@
 
     <!-- Bootstrap core CSS -->
     <link href="./css/bootstrap.min.css" rel="stylesheet">
+    <link href="./css/style.css" rel="stylesheet">
+    <link rel="stylesheet" href="./css/font-awesome.min.css">
   </head>
-
+  
 <nav class="navbar navbar-default">
   <div class="container-fluid">
     <!-- Brand and toggle get grouped for better mobile display -->
@@ -40,23 +42,64 @@
       <form class="navbar-form navbar-right">
         <div class="form-group">
           <input type="text" class="form-control" placeholder="Search">
+        
+        <a href="#" ><i class="fa fa-bell-o fa-fw"></i></a>
+        <img herf="#" class="img-circle" src="image/photo.jpg" alt="photo" width="30" height="30">
         </div>
-        <button type="submit" class="btn btn-default">Submit</button>
       </form>
   </div><!-- /.container-fluid -->
 </nav>
-<div class="container">
+<div class="container insidecontainer">
 	
 	<div class="row">
 	  	<div class="col-sm-3">
+		  	
 		  	<div class="panel panel-default">
-		  	<div>
-		  		<img class="img-responsive" src="image/turtle.jpg" alt="turtle">
-		  	</div>	
+			  	<div>
+			  		<img class="img-responsive" src="image/turtle.jpg" alt="turtle">
+			  	</div>	
+			  	<div>
+		  		<img class="img-circle" src="image/turtle.jpg" alt="turtle" width="120" height="120">
+		  		</div>
 		  		<div class="panel-body">
-		    		Basic panel example
+		    		<strong> Mingyang Yu </strong> <br>
+		    		<p> sjai fwqoia wjqiorasn wqroi qwbas a rwqijoran sjakwrq sfjiqo sakalfq f fqiohfa qhiroq</p>
+		    	</div>
+		    	<div class="span1">
+				    <div class="mycontent-left">
+				    Friends <br> <strong>12M</strong>
+				    </div>
+				  </div>
+				  <div class="span1">
+				    <div class="mycontent-right">
+				    Enemis <br> <strong>1</strong>
+				    </div>
+				</div>
+		  	</div>
+
+		  	<div class="panel panel-default">
+		  		<div class="panel-body">
+		    		<STRONG>ABOUT</STRONG> &middot <a href=#> Edit</a><br><br>
+		    		<i class="fa fa-calendar-o fa-fw"></i> Went to <a href="#"> Oh, Canada</a><br>
+		    		<i class="fa fa-users fa-fw"></i> Became friends with <a href="#">Obama</a><br>
+		    		<i class="fa fa-github-alt fa-fw"></i> Worked at <a href="#">Github</a><br>
+		    		<i class="fa fa-home fa-fw"></i> Lives in <a href="#">San Francisco, CA</a><br>
+		    		<i class="fa fa-map-marker fa-fw"></i> From <a href="#">Seattle, WA</a><br>
 		    	</div>
 		  	</div>
+
+		  	<div class="panel panel-default">
+		  		<div class="panel-body">
+		    		<STRONG>Photos</STRONG> <a href> Edit</a><br><br>
+		    		<img src="image/chicks.jpg" alt="goose" width="110" height="110">
+					<img src="image/ham.jpg" alt="goose" width="110" height="110"><br>		    	
+					<img src="image/seal.jpg" alt="goose" width="110" height="110">
+					<img src="image/photo.jpg" alt="goose" width="110" height="110"><br>
+					<img src="image/turtle.jpg" alt="goose" width="110" height="110">
+		    	</div>
+		  	</div>
+			
+
 		</div>
   
 	  	<div class="col-sm-6">
@@ -65,61 +108,132 @@
 			    	<div class="input-group">
 				      <input type="text" class="form-control" placeholder="Message">
 				    	<span class="input-group-btn">
-				        <button class="btn btn-default" type="button">Camera</button>
+				        <button class="btn btn-default" type="button"><a href="#" ><i class="fa fa-camera"></i></a></button>
 				      	</span>
+				      	
 				    </div>
 
-				    <div>
+				    	<div>
+						    <ul class="list-group ">
+							    <li class="list-group-item borderless">	
+								    <div class="media">
+									  <div class="media-left">
+									      <img class="media-object" src="image/goose.jpg" alt="goose" width="120" height="120">
+									  </div>
+									  <div class="media-body">
+									    <h4 class="media-heading">Goose</h4>
 
-				    <div class="media">
-					  <div class="media-left">
-					    <a href="#">
-					      <img class="media-object" src="image/goose.jpg" alt="goose">
-					    </a>
-					  </div>
-					  <div class="media-body">
-					    <h4 class="media-heading">Goose</h4>
-					    Aenean lacinia bibendum nulla sed consectetur. Vestibulum id ligula porta felis euismod semper. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-					  </div>
-					</div>
-					<div class="media">
-					  <div class="media-left">
-					    <a href="#">
-					      <img class="media-object" src="image/goose.jpg" alt="goose">
-					    </a>
-					  </div>
-					  <div class="media-body">
-					    <h4 class="media-heading">Goose</h4>
-					    Aenean lacinia bibendum nulla sed consectetur. Vestibulum id ligula porta felis euismod semper. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-					  </div>
-					</div>
-					<div class="media">
-					  <div class="media-left">
-					    <a href="#">
-					      <img class="media-object" src="image/goose.jpg" alt="goose">
-					    </a>
-					  </div>
-					  <div class="media-body">
-					    <h4 class="media-heading">Goose</h4>
-					    Aenean lacinia bibendum nulla sed consectetur. Vestibulum id ligula porta felis euismod semper. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-					  </div>
-					</div>
-				    </div>
-
-
+									    Aenean lacinia bibendum nulla sed consectetur. Vestibulum id ligula porta felis euismod semper. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.<br>
+									    <img src="image/chicks.jpg" alt="goose" width="110" height="110">
+										<img src="image/ham.jpg" alt="goose" width="110" height="110">		    	
+										<img src="image/seal.jpg" alt="goose" width="110" height="110">
+										<img src="image/photo.jpg" alt="goose" width="110" height="110">
+										<img src="image/turtle.jpg" alt="goose" width="110" height="110">
+										<div class="media">
+										  <div class="media-left">
+										    <a href="#">
+										      <img class="media-object" src="image/goose.jpg" alt="goose" width="70" height="70">
+										    </a>
+										  </div>
+										  <div class="media-body">
+										    <h4 class="media-heading">Goose</h4>
+										    Aenean lacinia 
+										  </div>
+										</div>
+										<div class="media">
+										  <div class="media-left">
+										    <a href="#">
+										      <img class="media-object" src="image/goose.jpg" alt="goose" width="70" height="70">
+										    </a>
+										  </div>
+										  <div class="media-body">
+										    <h4 class="media-heading">Goose</h4>
+										    Aenean lacinia bibendum nulla sed consectetur. Vestibulum id ligula porta fe
+										  </div>
+										</div>
+									  </div>
+									</div>
+								</li>
+								<li class="list-group-item borderless">
+									<div class="media">
+									  <div class="media-left">
+									    <a href="#">
+									      <img class="media-object" src="image/goose.jpg" alt="goose" width="120" height="120">
+									    </a>
+									  </div>
+									  <div class="media-body">
+									    <h4 class="media-heading">Goose</h4>
+									    Aenean lacinia bibendum nulla sed consectetur. Vestibulum id ligula porta felis euismod semper. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+									  </div>
+									</div>
+								</li>
+								<li class="list-group-item borderless">
+									<div class="media">
+									  <div class="media-left">
+									    <a href="#">
+									      <img class="media-object" src="image/goose.jpg" alt="goose" width="120" height="120">
+									    </a>
+									  </div>
+									  <div class="media-body">
+									    <h4 class="media-heading">Goose</h4>
+									    Aenean lacinia bibendum nulla sed consectetur. Vestibulum id ligula porta felis euismod semper. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+									  </div>
+									</div>
+								</li>
+							</ul>	
+					  	</div>
 			  	</div>
 			</div>
 	  	</div>
 	  	
 	  	<div class="col-sm-3">
-	  		<div class="panel panel-default">
-	  			<div class="panel-body">
-	    			<div class="alert alert-warning alert-dismissible" role="alert">
+
+	    	<div class="alert alert-warning alert-dismissible" role="alert">
   					<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
   					<strong>Visit your profile!</strong> Better check yourself, you're not looking too good.
-					</div>
-	  			</div>
 			</div>
+	  		
+	  	
+
+		  	<div class="panel panel-default">
+		  		<div class="panel-heading">Sponsored</div>
+		  		<div class="panel-body">
+		    		<img class="img-responsive" src="image/seal.jpg" alt="goose">
+		    		<br><strong>It might be time to visit Iceland.</strong><br>
+		    		<p> Iceland is so chill, and everything looks cool here. Also, we heard the people are pretty nice. What are you waiting for?</p>
+		    	</div>
+		    	<div class="panel-footer">
+		    	<button type="button" class="btn btn-default btn-sm">Buy a ticket</button>
+		    	</div>
+		  	</div>	
+
+		  	<div class="panel panel-default">
+		  		<div class="panel-body">
+		    		<strong>Likes</strong><a href="#">View All</a><br>
+		    		<div class="media">
+						<div class="media-left">
+					      	<img class="media-object" src="image/photo.jpg" alt="goose" width="50" height="50">
+						</div>
+						<div class="media-body">
+						<strong>Jacob Thornton</strong>@fat<br>
+						<button type="button" class="btn btn-default btn-sm"><i class="fa fa-user-plus" aria-hidden="true"></i>Follow</button>
+						</div>
+		    		</div>
+		    		<div class="media">
+						<div class="media-left">
+					      	<img class="media-object" src="image/seal.jpg" alt="goose" width="50" height="50">
+						</div>
+						<div class="media-body">
+						<strong>Mark Otto</strong>@mdo<br>
+						<button type="button" class="btn btn-default btn-sm"><i class="fa fa-user-plus" aria-hidden="true"></i>Follow</button>
+						</div>
+		    		</div>
+		  		</div>
+		  		<div class="panel-footer">
+		    		<p>Dave really likes these nerds, no one knows why though.</p>
+		    	</div>
+
+
 	  	</div>
 	</div>
 
