4a3b560537e178878758ee21609a150322c8898e
ymy90911 <ymy90911@hotmail.com>
2017-06-27 08:06:38 -0500

    First commit

---
+++ practice.php
@@ -0,0 +1,143 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+
+    <title>Will's Practice</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="./css/bootstrap.min.css" rel="stylesheet">
+  </head>
+
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">OOOO</a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <!--div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1"-->
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">Home <span class="sr-only">(current)</span></a></li>
+        <li><a href="#">Profile</a></li>
+        <li><a href="#">Messages</a></li>
+        <li><a href="#">Docs</a></li>
+
+      </ul>
+      <form class="navbar-form navbar-right">
+        <div class="form-group">
+          <input type="text" class="form-control" placeholder="Search">
+        </div>
+        <button type="submit" class="btn btn-default">Submit</button>
+      </form>
+  </div><!-- /.container-fluid -->
+</nav>
+<div class="container">
+	
+	<div class="row">
+	  	<div class="col-sm-3">
+		  	<div class="panel panel-default">
+		  	<div>
+		  		<img class="img-responsive" src="image/turtle.jpg" alt="turtle">
+		  	</div>	
+		  		<div class="panel-body">
+		    		Basic panel example
+		    	</div>
+		  	</div>
+		</div>
+  
+	  	<div class="col-sm-6">
+	  		<div class="panel panel-default">
+				<div class="panel-body">
+			    	<div class="input-group">
+				      <input type="text" class="form-control" placeholder="Message">
+				    	<span class="input-group-btn">
+				        <button class="btn btn-default" type="button">Camera</button>
+				      	</span>
+				    </div>
+
+				    <div>
+
+				    <div class="media">
+					  <div class="media-left">
+					    <a href="#">
+					      <img class="media-object" src="image/goose.jpg" alt="goose">
+					    </a>
+					  </div>
+					  <div class="media-body">
+					    <h4 class="media-heading">Goose</h4>
+					    Aenean lacinia bibendum nulla sed consectetur. Vestibulum id ligula porta felis euismod semper. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+					  </div>
+					</div>
+					<div class="media">
+					  <div class="media-left">
+					    <a href="#">
+					      <img class="media-object" src="image/goose.jpg" alt="goose">
+					    </a>
+					  </div>
+					  <div class="media-body">
+					    <h4 class="media-heading">Goose</h4>
+					    Aenean lacinia bibendum nulla sed consectetur. Vestibulum id ligula porta felis euismod semper. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+					  </div>
+					</div>
+					<div class="media">
+					  <div class="media-left">
+					    <a href="#">
+					      <img class="media-object" src="image/goose.jpg" alt="goose">
+					    </a>
+					  </div>
+					  <div class="media-body">
+					    <h4 class="media-heading">Goose</h4>
+					    Aenean lacinia bibendum nulla sed consectetur. Vestibulum id ligula porta felis euismod semper. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+					  </div>
+					</div>
+				    </div>
+
+
+			  	</div>
+			</div>
+	  	</div>
+	  	
+	  	<div class="col-sm-3">
+	  		<div class="panel panel-default">
+	  			<div class="panel-body">
+	    			<div class="alert alert-warning alert-dismissible" role="alert">
+  					<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+  					<strong>Visit your profile!</strong> Better check yourself, you're not looking too good.
+					</div>
+	  			</div>
+			</div>
+	  	</div>
+	</div>
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
